583c0674f957105f63665ef0e3d6fbd322979352
Albert <albert0891@gmail.com>
2025-11-06 23:25:39 -0500

    feat: Initial project setup for Job Tracker Api

--- JobTracker.Api/obj/Debug/net9.0/JobTracker.Api.AssemblyInfo.cs
+++ JobTracker.Api/obj/Debug/net9.0/JobTracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab80c9d34247f7ae31bc71dfc2981258e2520f2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
